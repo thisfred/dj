7949d8756b9c365807ac18a82102f32ac290d22f
Eric Casteleijn <thisfred@gmail.com>
2020-04-26 15:53:50 -0700

    Small proof of concept.
    This serializes and deserializes plain (type annotated) dataclasses,
    from and to JSON.
    
    Supports: (These are just some interesting examples I thought up.)
    
      * Simple types (str, int, float, bool)
      * Default values for fields. (for free through dataclass __init__)
      * Enums
      * dates
      * Optional and Union of the above
    
    This likely has bugs, and it's unlikely to stay this small, if I decide
    to make it into a production ready library.

--- src/dj/__init__.py
+++ src/dj/__init__.py
@@ -4,28 +4,20 @@ from dataclasses import asdict, fields
 from datetime import date
 from enum import Enum
 from functools import singledispatch
-from typing import Any, Mapping, Protocol, Union
+from typing import Any, Iterable, Mapping, Protocol, Type, Union, runtime_checkable
 
 
-@singledispatch
-def to_serializable(val: Any) -> str:
-    """
-    Neat trick copied from Hynek Schlawack's blog post:
-
-    https://hynek.me/articles/serialization/
-    """
-    return str(val)
+class ValidationError(Exception):
+    """Validation error for deserializing JSON into dataclasses."""
 
 
-@to_serializable.register
-def serialize_date(value: date, /) -> str:
-    return value.isoformat()
+SIMPLE_TYPES = {str, int, bool, type(None)}
 
 
-@to_serializable.register
-def serialize_enum(enum: Enum) -> Union[str, int]:
-    result: Union[str, int] = enum.value
-    return result
+@runtime_checkable
+class GenericAlias(Protocol):
+    __origin__: Type[Any]
+    __args__: Iterable[Any]
 
 
 class DataClassInstance(Protocol):
@@ -35,17 +27,9 @@ class DataClassInstance(Protocol):
 def from_json(json_input: str, cls: Any) -> DataClassInstance:
     raw = json.loads(json_input)
     for field in fields(cls):
-        if field.type in (str, int, bool):
-            assert isinstance(raw[field.name], field.type)
-            continue
-
-        if field.type is date:
-            raw[field.name] = date.fromisoformat(raw[field.name])
-            continue
-
-        if issubclass(field.type, Enum):
-            raw[field.name] = field.type(raw[field.name])
-            continue
+        raw[field.name] = validate_and_deserialize(
+            field.type, raw.get(field.name), field.name
+        )
 
     instance: DataClassInstance = cls(**raw)
     return instance
@@ -53,3 +37,57 @@ def from_json(json_input: str, cls: Any) -> DataClassInstance:
 
 def to_json(instance: DataClassInstance) -> str:
     return json.dumps(asdict(instance), default=to_serializable)
+
+
+@singledispatch
+def to_serializable(value: Any, /) -> str:
+    """
+    Neat trick copied from Hynek Schlawack's blog post:
+
+    https://hynek.me/articles/serialization/
+    """
+    return str(value)
+
+
+@to_serializable.register
+def serialize_date(value: date, /) -> str:
+    return value.isoformat()
+
+
+@to_serializable.register
+def serialize_enum(enum: Enum, /) -> Union[str, int]:
+    result: Union[str, int] = enum.value
+    return result
+
+
+def validate_and_deserialize(  # noqa
+    field_type: Any, value: Any, field_name: str
+) -> Any:
+    # This has a high cyclomatic complexity, but splitting it up would lead to more
+    # indirection than this short function merits. Will revisit as it grows.
+    if field_type in SIMPLE_TYPES:
+        if isinstance(value, field_type):
+            return value
+
+    if field_type is date:
+        return date.fromisoformat(value)
+
+    if isinstance(field_type, GenericAlias):
+        if field_type.__origin__ == Union:
+            for possible_type in field_type.__args__:
+                try:
+                    return validate_and_deserialize(possible_type, value, field_name)
+                except ValidationError:
+                    continue
+
+    try:
+        if issubclass(field_type, Enum):
+            return field_type(value)
+
+    except TypeError:
+        pass
+
+    raise ValidationError(
+        f"Field `{field_name}` got value of unexpected type: {type(value)}, should be: "
+        f"{field_type}."
+    )
--- tests/dj/test_dj.py
+++ tests/dj/test_dj.py
@@ -1,10 +1,13 @@
+import json
+
 from dataclasses import dataclass
 from datetime import date
 from enum import Enum
+from typing import Optional
 
-from pytest import fixture
+from pytest import fixture, raises
 
-from dj import from_json, to_json
+from dj import ValidationError, from_json, to_json
 
 
 class ReleaseType(Enum):
@@ -17,8 +20,9 @@ class ReleaseType(Enum):
 class Record:
     artist: str
     title: str
-    release_type: ReleaseType
-    release_date: date
+    release_type: Optional[ReleaseType]
+    release_date: Optional[date]
+    notes: Optional[str] = None
 
 
 @fixture
@@ -33,3 +37,11 @@ def a_record() -> Record:
 
 def test_roundtrip(a_record):
     assert from_json(to_json(a_record), Record) == a_record
+
+
+def test_raises_nice_exceptions(a_record):
+    broken_record = json.loads(to_json(a_record))
+    broken_record["title"] = 12
+
+    with raises(ValidationError, match="title"):
+        from_json(json.dumps(broken_record), Record)
